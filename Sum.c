1ea5fe9997270bd01e8e9c028aafb95e1b81ce64
PepGuiN <pepper00023@users.noreply.github.com>
2017-04-27 21:00:58 +0700

    Sum.c is changed

--- Sum.c
+++ Sum.c
@@ -2,9 +2,9 @@
 int main()
 {
 	double a,b;
-	printf("Enter 1st number ==> ");
+	printf("Enter 1st number => ");
 	scanf("%lf",&a);
-	printf("Enter 2nd number ==> ");
+	printf("Enter 2nd number => ");
 	scanf("%lf",&b);
 	printf("Sum = %lf\n",a+b);
 	return 0;
